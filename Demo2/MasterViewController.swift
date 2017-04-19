22411a76eea76ca193231faf085fc579b6467b67
Richard de los Santos <richarddelossantos@AI-MacBook-Pro.local>
2017-04-18 22:16:22 -0400

    fixed issue where table was not loading consistently due to the NSURLSession response times

--- Demo2/MasterViewController.swift
+++ Demo2/MasterViewController.swift
@@ -8,6 +8,48 @@
 
 import UIKit
 
+//http://stackoverflow.com/questions/29912852/how-to-show-activity-indicator-while-tableview-loads
+fileprivate var ActivityIndicatorViewAssociativeKey = "ActivityIndicatorViewAssociativeKey"
+public extension UIView {
+    var activityIndicatorView: UIActivityIndicatorView {
+        get {
+            if let activityIndicatorView = getAssociatedObject(&ActivityIndicatorViewAssociativeKey) as? UIActivityIndicatorView {
+                return activityIndicatorView
+            } else {
+                let activityIndicatorView = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
+                activityIndicatorView.activityIndicatorViewStyle = .gray
+                activityIndicatorView.color = .gray
+                activityIndicatorView.center = center
+                activityIndicatorView.hidesWhenStopped = true
+                addSubview(activityIndicatorView)
+                
+                setAssociatedObject(activityIndicatorView, associativeKey: &ActivityIndicatorViewAssociativeKey, policy: .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+                return activityIndicatorView
+            }
+        }
+        
+        set {
+            addSubview(newValue)
+            setAssociatedObject(newValue, associativeKey:&ActivityIndicatorViewAssociativeKey, policy: .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+}
+
+public extension NSObject {
+    func setAssociatedObject(_ value: AnyObject?, associativeKey: UnsafeRawPointer, policy: objc_AssociationPolicy) {
+        if let valueAsAnyObject = value {
+            objc_setAssociatedObject(self, associativeKey, valueAsAnyObject, policy)
+        }
+    }
+    
+    func getAssociatedObject(_ associativeKey: UnsafeRawPointer) -> Any? {
+        guard let valueAsType = objc_getAssociatedObject(self, associativeKey) else {
+            return nil
+        }
+        return valueAsType
+    }
+}
+
 open class Organization: NSObject {
 
     var name  = String()
@@ -21,14 +63,104 @@ open class Organization: NSObject {
     
 }
 
-class MasterViewController: UITableViewController {
+class MasterViewController: UITableViewController, NSURLConnectionDelegate, UISearchBarDelegate {
 
     var detailViewController = DetailViewController()
     var objects = [Organization]()
     var sort_type = 0   // 0 - A-Z, 1 - Newest
-    
+
     var sortButtonAZ     = UIBarButtonItem()
     var sortButtonNewest = UIBarButtonItem()
+    var searchButton = UIBarButtonItem()
+    
+    var server_data = [Organization]()
+    
+    var searchbar = UISearchBar()
+    var menu_toolbar = UIToolbar()
+    
+    var menu_toolbar_state = CGFloat(1.0)
+    
+    func toggleSearch() {
+        
+        let offset = CGFloat(44.0)
+        self.menu_toolbar_state *= CGFloat(-1.0)
+        
+        UIView.animate(withDuration: 0.25) {
+            self.menu_toolbar.center.y = self.menu_toolbar.center.y + offset*self.menu_toolbar_state
+        }
+        
+        if(self.menu_toolbar_state == -1.0) {
+            self.searchbar.becomeFirstResponder()
+            
+            self.searchButton.setTitleTextAttributes(
+                [
+                    NSFontAttributeName : UIFont(name: "HelveticaNeue-Bold", size: 18)!
+                ],
+                for: .normal)
+        }
+        else {
+            
+            self.searchButton.setTitleTextAttributes(
+                [
+                    NSFontAttributeName : UIFont(name: "HelveticaNeue", size: 18)!
+                ],
+                for: .normal)
+            
+            self.searchbar.resignFirstResponder()
+            
+            self.searchbar.text = ""
+            
+            objects = self.server_data
+            
+            self.tableView.reloadData()
+            
+        }
+        
+    }
+    
+    func filterOrganizations(searchText: String) {
+        
+        objects = self.server_data.filter{ (($0 as! Organization).name.lowercased().contains(searchText.lowercased())) }
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchText != "" {
+            self.filterOrganizations(searchText: searchText)
+        }
+        else {
+            objects = self.server_data
+            
+        }
+        
+        self.tableView.reloadData()
+        
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        
+        
+        
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        
+        self.toggleSearch()
+        
+        objects = self.server_data
+        
+        self.sortList()
+        
+        self.tableView.reloadData()
+        
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,6 +180,9 @@ class MasterViewController: UITableViewController {
     override func viewWillAppear(_ animated: Bool) {
       //  clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
+        
+        self.getServerData()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -61,7 +196,7 @@ class MasterViewController: UITableViewController {
             objects = objects.sorted(by: { $0.name < $1.name })
         }
         if sort_type == 1 {
-            objects = objects.sorted(by: { $0.name > $1.name })
+            objects = objects.sorted(by: { $0.time_stamp > $1.time_stamp })
 
 //            objects = objects.sorted(by: { $0.time_stamp > $1.time_stamp })
         }
@@ -166,9 +301,15 @@ class MasterViewController: UITableViewController {
         
         view.addSubview(webView)
         
-        let toolbar = UIToolbar(frame: CGRect(x: 0.0, y: view.frame.size.height-44.0, width:self.view.frame.size.width, height: 44.0))
+        self.searchbar = UISearchBar(frame: CGRect(x:0.0, y: view.frame.size.height-44, width: self.view.frame.width, height:44))
+        self.searchbar.delegate = self
+        self.searchbar.showsCancelButton = true
         
-        self.sortButtonAZ = UIBarButtonItem(title: "A-Z", style: UIBarButtonItemStyle.done, target: self, action: "setSortAZ")
+        view.addSubview(searchbar)
+        
+        self.menu_toolbar = UIToolbar(frame: CGRect(x: 0.0, y: view.frame.size.height-44.0, width:self.view.frame.size.width, height: 44.0))
+        
+        self.sortButtonAZ = UIBarButtonItem(title: "A-Z", style: UIBarButtonItemStyle.done, target: self, action: #selector(MasterViewController.setSortAZ))
         
         // http://stackoverflow.com/questions/8849913/how-can-i-change-font-of-uibarbuttonitem
         self.sortButtonAZ.setTitleTextAttributes(
@@ -177,21 +318,72 @@ class MasterViewController: UITableViewController {
             ],
             for: .normal)
         
-        self.sortButtonNewest = UIBarButtonItem(title: "NEWEST", style: UIBarButtonItemStyle.plain, target: self, action: "setSortNewest")
-        let button3 = UIBarButtonItem(title: "SEARCH", style: UIBarButtonItemStyle.plain, target: self, action: nil)
+        self.sortButtonNewest = UIBarButtonItem(title: "NEWEST", style: UIBarButtonItemStyle.plain, target: self, action: #selector(MasterViewController.setSortNewest))
+        self.searchButton = UIBarButtonItem(title: "SEARCH", style: UIBarButtonItemStyle.plain, target: self, action: #selector(MasterViewController.toggleSearch))
         
         self.sortButtonAZ.tintColor = UIColor.gray
         sortButtonNewest.tintColor  = UIColor.gray
-        button3.tintColor           = UIColor.gray
+        self.searchButton.tintColor           = UIColor.gray
         
         // https://www.hackingwithswift.com/example-code/uikit/how-to-add-a-flexible-space-to-a-uibarbuttonitem
         let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         
-        toolbar.items = [spacer,self.sortButtonAZ,spacer,self.sortButtonNewest,spacer,button3,spacer]
+        menu_toolbar.items = [spacer,self.sortButtonAZ,spacer,self.sortButtonNewest,spacer,self.self.searchButton,spacer]
         
-        view.addSubview(toolbar)
+        view.addSubview(menu_toolbar)
         
         return view
+        
+    }
+
+    // http://stackoverflow.com/questions/24065536/downloading-and-parsing-json-in-swift
+    func getServerData(){
+        
+        self.tableView.activityIndicatorView.startAnimating()
+        
+        // http://stackoverflow.com/a/35586622
+        // Asynchronous Http call to your api url, using NSURLSession:
+        URLSession.shared.dataTask(with: NSURL(string: "https://gist.githubusercontent.com/radls/d02f30018ed3ba6ab955861469f66906/raw/c30f22d95cfcd43836b8f3c7daffe564f9c07dc1/Locations.json")! as URL, completionHandler: { (data, response, error) -> Void in
+            // Check if data was received successfully
+            if error == nil && data != nil {
+                do {
+                    // Convert NSData to Dictionary where keys are of type String, and values are of any type
+                    let json = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Array<NSDictionary>
+                    
+                    for organization in json {
+                        
+                        let new_org = Organization()
+                        new_org.name = organization.value(forKey: "name") as! String
+                        new_org.location = organization.value(forKey: "location") as! String
+                        new_org.url = organization.value(forKey: "url") as! String
+                        new_org.phone = organization.value(forKey: "phone") as! String
+                        new_org.email = organization.value(forKey: "email") as! String
+                        new_org.time_stamp = Date()
+                        
+                        self.objects.insert(new_org, at: 0)
+                        //   let indexPath = IndexPath(row: 0, section: 0)
+                        //   self.tableView.insertRows(at: [indexPath], with: .automatic)
+                        
+                    }
+                    
+                    self.tableView.activityIndicatorView.stopAnimating()
+                    
+                    self.server_data = self.objects
+                    
+                    self.tableView.reloadData()
+                    
+                    // print(self.objects)
+                    
+                    // print(json)
+                    // print(json.count)
+                    // print(json[0].value(forKey: "name") as! String)
+                    
+                } catch {
+                    print(error)
+                    // Something went wrong
+                }
+            }
+        }).resume()
         
     }
 
@@ -222,6 +414,12 @@ class MasterViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            
+            // http://stackoverflow.com/questions/28727845/find-an-object-in-array
+            if let found = server_data.index(where: {$0 as! Organization == objects[indexPath.row] as! Organization}) {
+                server_data.remove(at: found)
+            }
+
             objects.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
